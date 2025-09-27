cc10448adf4b74cda84bc1149489510c43541ce4
Nickdrem <160512535+JoanBiel2@users.noreply.github.com>
2025-09-27 21:32:39 +0200

    Lo del gmail funciona

--- public/index.php
+++ public/index.php
@@ -1,4 +1,10 @@
 <?php
+require __DIR__ . '/../PHP/phpmailer/src/Exception.php';
+require __DIR__ . '/../PHP/phpmailer/src/PHPMailer.php';
+require __DIR__ . '/../PHP/phpmailer/src/SMTP.php';
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
 
 // Iniciem una nova sessió o continuem la que hi ha
 session_start();
@@ -10,6 +16,7 @@ $db_connection = 'sqlite:..\private\users.db';
 $configuration = array(
     '{FEEDBACK}'          => '',
     '{LOGIN_LOGOUT_TEXT}' => 'Identificar-me',
+    '{RECOVERY_URL}'       => '/?page=recovery',
     '{LOGIN_LOGOUT_URL}'  => '/?page=login',
     '{METHOD}'            => 'POST', // es veuen els paràmetres a l'URL i a la consola (???)
     '{REGISTER_URL}'      => '/?page=register',
@@ -27,7 +34,78 @@ if (isset($_SESSION['user_mail'])) {
 $parameters = array_merge($_GET, $_POST);
 
 // Primer, processar intents de registre o login (POST)
-if (isset($parameters['register'])) {
+if (isset($parameters['recovery'])) {
+    // Procesar recuperación de contraseña
+    $db = new PDO($db_connection);
+    $sql = 'SELECT * FROM users WHERE user_mail = :user_mail';
+    $query = $db->prepare($sql);
+    $query->bindValue(':user_mail', $parameters['user_mail']);
+    $query->execute();
+    $user_mail = $query->fetchObject();
+    if ($user_mail) {
+    $configuration['{FEEDBACK}'] = 'T&apos;hem enviat un correu amb instruccions per canviar la contrasenya.';
+
+    // antes: $mail = new PHPMailer(true);
+$mail = new PHPMailer(true);
+
+// debug collector
+$debug = '';
+
+try {
+    $mail->SMTPDebug = 3; // 0=off, 1=client, 2=client+server, 3/4 = muy verboso
+    $mail->Debugoutput = function($str, $level) use (&$debug) {
+        $debug .= nl2br(htmlspecialchars($str)) . "<br>\n";
+    };
+
+    $mail->isSMTP();
+    $mail->Host       = 'smtp.gmail.com';
+    $mail->SMTPAuth   = true;
+    $mail->Username   = 'brostortillamario@gmail.com';
+    $mail->Password   = 'sehkqoxuheaphlzk';
+    // prueba STARTTLS/587 primero:
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+    $mail->Port       = 587;
+
+    // Si 587 falla, prueba 465 (SMTPS)
+    // $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
+    // $mail->Port = 465;
+
+    // Opcional (solo para desarrollo local si tienes problemas con certificados)
+    // NO usar en producción:
+    $mail->SMTPOptions = [
+        'ssl' => [
+            'verify_peer' => false,
+            'verify_peer_name' => false,
+            'allow_self_signed' => true
+        ]
+    ];
+
+    $mail->setFrom('brostortillamario@gmail.com', 'La meva pàgina');
+    $mail->addAddress($parameters['user_mail']);
+    $mail->isHTML(true);
+    $mail->Subject = 'Canvi de contrasenya';
+    $mail->Body    = "Hola,<br><br>Per canviar la teva contrasenya, visita aquest enllaç:<br>
+        <a href='http://localhost:8080/reset_password.php?mail=" . urlencode($parameters['user_mail']) . "'>Canviar contrasenya</a>";
+
+    $mail->send();
+    $configuration['{FEEDBACK}'] = 'Correu enviat correctament.';
+} catch (Exception $e) {
+    // Muestra tanto ErrorInfo como la excepción y el debug SMTP
+    $msg = 'No s\'ha pogut enviar el correu. PHPMailer Error: ' . $mail->ErrorInfo;
+    $msg .= '<br>Exception: ' . htmlspecialchars($e->getMessage());
+    $msg .= '<br>Debug:<br>' . $debug;
+    $configuration['{FEEDBACK}'] = $msg;
+}
+
+} else {
+        $configuration['{FEEDBACK}'] = '<mark>ERROR: No existeix cap compte amb aquest correu</mark>';
+    }
+    $template = 'recovery';
+    $configuration['{RECOVERY_MAIL}'] = htmlentities($parameters['user_mail']);
+    $configuration['{RECOVERY_URL}'] = '/?page=recovery';
+    $configuration['{LOGIN_URL}'] = '/?page=login';
+    $configuration['{REGISTER_URL}'] = '/?page=register';
+} else if (isset($parameters['register'])) {
     // Connexió a la base de dades per registrar nou usuari
     $db = new PDO($db_connection);
 
@@ -119,6 +197,13 @@ if (isset($parameters['register'])) {
             $configuration['{LOGIN_LOGOUT_TEXT}'] = 'Identificar-me';
             $configuration['{LOGIN_LOGOUT_URL}'] = '/?page=login';
             $template = 'home';
+        } else if ($parameters['page'] == 'recovery') {
+            // Mostrar formulario de recuperación
+            $template = 'recovery';
+            $configuration['{RECOVERY_MAIL}'] = '';
+            $configuration['{RECOVERY_URL}'] = '/?page=recovery';
+            $configuration['{LOGIN_URL}'] = '/?page=login';
+            $configuration['{REGISTER_URL}'] = '/?page=register';
         }
     }
 }
